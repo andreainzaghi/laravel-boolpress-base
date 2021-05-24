4256af09fd8c40476b4fa225ee2d9ed339d71eec
Andrea <andrea3aprile99@gmail.com>
2021-05-24 16:56:25 +0200

    facker comment

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Comment;
+use App\Post;
 class CommentsTableSeeder extends Seeder
 {
     /**
@@ -15,7 +16,19 @@ class CommentsTableSeeder extends Seeder
        $posts = Post::where('published', 1)->get();
        
        foreach ($posts as  $post) {
-           for($i =0 ;$i< rand(0,3) ; $i++)
+           for($i =0 ;$i< rand(0,3) ; $i++){
+               $newComment = new Comment();
+               $newComment -> post_id = $post ->id;
+               if(rand(0,1)){
+                   $newComment-> name = $faker -> name();
+
+               }
+               $newComment -> content = $faker ->text;
+               $newComment -> save();
+
+
+
+           }
        }
     }
 }
